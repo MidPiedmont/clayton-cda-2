07d53ec66333aaf0c5faed6d5d7968393716bda3
MidPiedmont <rjzmanz@gmail.com>
2025-03-18 16:20:47 -0500

    garbage

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react';
 import App from './App';
 import { BrowserRouter } from "react-router";
 import ReactGA from 'react-ga4';
